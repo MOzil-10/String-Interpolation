1b97178778534611d978949f84b76ef7345f9831
MOzil-10 <mukosibudeli@gmail.comm>
2023-07-07 00:41:17 +0200

    added the file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'stringInterpolation';
+
+
+public name = "Ozil";
+
+constructor(){}
+  ngOnInit(): void {
+   
+  }
+
+  greetUser(){
+    return "Hello " + this.name
+  }
+
 }
